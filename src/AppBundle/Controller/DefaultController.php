8975ec0d6c29654697d6e7165906159ddc0f1c18
Hugo Monsalve pelaz <hmonsalvepe@gmail.com>
2019-07-04 13:41:41 +0200

    Last commit

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -14,19 +14,54 @@ class DefaultController extends Controller
     public function indexAction(Request $request)
     {
 
-
         $rootPath = $this->get('kernel')->getRootDir();
 
-        $str = file_get_contents($rootPath.'/../web/swapiFiles/films.json');
-        $films = json_decode($str, true);
 
-        $filmsfile = $rootPath.'/../web/swapiFiles/films.json';
-        $charactersfile = $rootPath.'/../web/swapiFiles/people.json';
 
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', array(
+        /*
+            Here I decide to download JSON files to have the app more complete
+            If I get the JSON data from the url I only get 10 characters
+            The way to get the info from a URL would be:
 
-        ));
+            Next is to avoid the SSL check, without it system won't get the info:
+
+            $arrContextOptions=array(
+                "ssl"=>array(
+                    "verify_peer"=>false,
+                    "verify_peer_name"=>false,
+                ),
+            );
+
+            $json = file_get_contents("http://swapi.co/api/people", false, stream_context_create($arrContextOptions));
+            $obj = json_decode($json);
+
+        */
+        $charactersFile = file_get_contents($rootPath.'/../web/swapiFiles/people.json');
+        $characters = json_decode($charactersFile, true);
+
+        $filmsFile = file_get_contents($rootPath.'/../web/swapiFiles/films.json');
+        $films = json_decode($filmsFile, true);
+
+        foreach ($films as $film){
+            $allFilmCharacters[$film['episode_id']] = $film['characters'];
+        }
+
+        foreach ($allFilmCharacters as $episodeID => $filmCharacters){
+
+            foreach ($characters as $character){
+
+                foreach ($filmCharacters as $filmCharacter){
+
+                    if($filmCharacter == $character['url']){
+
+                        $charactersInFilms[$episodeID][] = $character;
+                    }
+                }
+            }
+        }
+
+        return $this->render('default/index.html.twig',
+            array( 'charactersInFilms' => $charactersInFilms));
     }
 
 
